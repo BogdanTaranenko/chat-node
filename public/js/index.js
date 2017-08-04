ec4ad189a130cf7c6188bdad0da319f29c0d423a
Bogdan Taranenko <bogdan13taranenko@gmail.com>
2017-08-05 01:51:45 +0300

    Add jQuery and form for show messages

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,4 @@
 //wrap in IIFE
-
-
     let socket = io();
 
 socket.on('connect', function ()  {
@@ -13,4 +11,19 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (message) {
     console.log("new message", message);
+    let li = $('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
+
+    $('#messages').append(li);
+});
+
+$('#form-message').on('submit', function (e) {
+   e.preventDefault();
+   let inputValue = $('[name=input-message]');
+   socket.emit('createMessage', {
+       from: "User",
+       text: inputValue.val()
+   }, function () {
+       inputValue.val('');
+   })
 });
